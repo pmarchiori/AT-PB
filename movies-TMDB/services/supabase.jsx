8bef9de31e0d0f1034848c3c88ec15f4a0fbff87
Pedro Marchiori <pmarchiori08@gmail.com>
2024-12-12 21:48:20 -0300

    fix

--- movies-TMDB/services/supabase.jsx
+++ movies-TMDB/services/supabase.jsx
@@ -3,9 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 import { AppState } from "react-native";
 import "react-native-url-polyfill";
 
-const supabaseUrl = "https://sdqylbyzxsnigybullis.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkcXlsYnl6eHNuaWd5YnVsbGlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwNDIxMjAsImV4cCI6MjA0OTYxODEyMH0.ANdNQkF8agjV_xQj6vdMF8clh8uX1tYXrG3vktrVnNw";
+const supabaseUrl = "";
+const supabaseAnonKey = "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
